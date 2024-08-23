6f4e62c192d819e68b5076817ffaf88f325b435e
Arnav Sharma <99024681+ItsArnavSh@users.noreply.github.com>
2024-08-23 19:23:43 +0530

    Update Home.tsx

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import Canvas from "./Canvas";
 const Home = () => {
   return (
     <div className="min-h-screen bg-cover bg-center bg-[url('https://i.pinimg.com/originals/2a/77/4d/2a774d34ddacf4a4b049721da7e1fe7f.gif')]">
@@ -7,6 +7,7 @@ const Home = () => {
       <button className="m-10 p-2 bg-blue-500 text-white rounded-lg">
         <Link to='/test'>Test</Link>
       </button>
+      <Canvas />
     </div>
   );
 }
