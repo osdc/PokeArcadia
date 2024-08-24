8329d587287226cc4d7dad1fc50d6e54e3ae24b5
ItsArnavSh <its.arnav.sh@gmail.com>
2024-08-24 18:06:17 +0530

    Initiating Shift

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-import Canvas from "./Canvas";
 const Home = () => {
   return (
     <div className="min-h-screen bg-cover bg-center bg-[url('https://i.pinimg.com/originals/2a/77/4d/2a774d34ddacf4a4b049721da7e1fe7f.gif')]">
-        <h1 className="text-black font-bold text-3xl flex justify-center items-center">Welcome to Open Sorcerer Developers Community</h1>
+      <h1 className="text-black font-bold text-3xl flex justify-center items-center">
+        Welcome to Open Sorcerer Developers Community
+      </h1>
       <button className="m-10 p-2 bg-blue-500 text-white rounded-lg">
-        <Link to='/test'>Test</Link>
+        <Link to="/test">Test</Link>
       </button>
     </div>
   );
-}
+};
 
 export default Home;
