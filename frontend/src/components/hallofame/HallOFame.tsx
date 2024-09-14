470683be9aec617d470861a3fe745f4120b07dea
ItsArnavSh <its.arnav.sh@gmail.com>
2024-09-14 19:53:49 +0530

    FIxed Tailwind

--- frontend/src/components/hallofame/HallOFame.tsx
+++ frontend/src/components/hallofame/HallOFame.tsx
@@ -1,6 +1,14 @@
 import Canvas from "./Canvas";
+import "../../index.css";
 const HallOFame = () => {
-  return <Canvas />;
+  return (
+    <>
+      {" "}
+      <div className="flex flex-col w-full h-lvh items-center justify-center">
+        <Canvas />
+      </div>
+    </>
+  );
 };
 
 export default HallOFame;
