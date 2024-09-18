12bb2261edc96355e88fdba4d4b2cf2d6b73693f
ItsArnavSh <its.arnav.sh@gmail.com>
2024-09-19 01:48:33 +0530

    Added 2 new screens

--- frontend/src/components/hallofame/HallOFame.tsx
+++ frontend/src/components/hallofame/HallOFame.tsx
@@ -4,7 +4,7 @@ const HallOFame = () => {
   return (
     <>
       {" "}
-      <div className="flex flex-col w-full h-lvh items-center justify-center">
+      <div className="flex flex-col w-full h-lvh items-center justify-center bg-[#6256BA] aspect-square">
         <Canvas />
       </div>
     </>
